73fdb8a573296e5d0d75e2e449af22c03eb1f335
Leo <leonoragilmour@sky.com>
2017-07-13 16:14:53 +0100

    enabling this widget to work with post type picker

--- widget-loader-acf.php
+++ widget-loader-acf.php
@@ -60,5 +60,5 @@ $widget_config = array (
   )
 );
 
-$widget_config["content-types"] = array('post', 'category');
+$widget_config["content-types"] = get_option("options_" . $key . "_available_post_types");
 $widget_config["content-sizes"] = array('main');
